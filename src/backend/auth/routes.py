11f6efe6753c5e0384f54344f62fe5ad48c470a4
BreNNoUchiha <142348444+brenouchihar@users.noreply.github.com>
2026-08-30 12:48:52 -0300

    feat/issue 45 teste brenouchihar homolog  (#69)
    * feat(auth): adiciona endpoint de teste da issue 45
    
    * fix(auth): corrige newline no teste da issue 45

--- src/backend/auth/routes.py
+++ src/backend/auth/routes.py
@@ -6,3 +6,12 @@ router = APIRouter()
 @router.get("/hello")
 def hello():
     return {"service": "auth", "status": "ok"}
+
+
+@router.get("/teste-brenouchihar")
+def teste_brenouchihar():
+    return {
+        "service": "auth",
+        "autor": "brenouchihar",
+        "mensagem": "hello world"
+    }
