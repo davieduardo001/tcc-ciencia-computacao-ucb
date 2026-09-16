879648dd977d624f7c3a151a34a06d8fed46587e
Kelvin963 <kelvin963@users.noreply.github.com>
2026-09-16 12:56:55 -0300

    fix: corrigir down_revision da migration para encadear no HEAD correto

--- src/backend/alembic/versions/b2c3d4e5f6a7_feat_criar_tabela_preferencias_notificacao.py
+++ src/backend/alembic/versions/b2c3d4e5f6a7_feat_criar_tabela_preferencias_notificacao.py
@@ -1,7 +1,7 @@
 """feat: criar tabela preferencias_notificacao
 
 Revision ID: b2c3d4e5f6a7
-Revises: a1b2c3d4e5f6
+Revises: f1a2b3c4d5e6
 Create Date: 2026-09-07 00:00:00.000000
 """
 from typing import Sequence, Union
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'b2c3d4e5f6a7'
-down_revision: Union[str, None] = 'a1b2c3d4e5f6'
+down_revision: Union[str, None] = 'f1a2b3c4d5e6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
